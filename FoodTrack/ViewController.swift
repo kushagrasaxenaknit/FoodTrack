d2076c0fa54472496d5ae2fbccdd4a90c29b3af9
Kushagra Saxena <kushagrasaxena@Kushagras-iMac.local>
2018-02-17 15:11:34 +0530

    Initial Commit

---
+++ FoodTrack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTrack
+//
+//  Created by Kushagra Saxena on 17/02/18.
+//  Copyright © 2018 Kushagra Saxena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
